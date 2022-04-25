cdfa030337bdc74bbdb79a7190e024cddc6bd782
Muhammad Zubair <zubairtnvr@outlook.com>
2022-04-25 15:53:01 +0300

    Amazon Demo Test Solution added with test cases

---
+++ CSConsole/Amazon/Demo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConsole.Amazon
+{
+    public static class Demo
+    {
+        public static int[] cellComplete(int[] states, int days)
+        {
+            int size = states.Length;
+            int[] Output = new int[size];
+
+            for (int i = 0; i < days; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (j <= 0)
+                    {
+                        //For First
+                        if (states[j + 1] == 0)
+                        {
+                            Output[j] = 0;
+                        }
+                        else
+                        {
+                            Output[j] = 1;
+                        }
+                    }
+                    else if (j >= (size - 1))
+                    {
+                        //For Last
+                        if (states[j - 1] == 0)
+                        {
+                            Output[j] = 0;
+                        }
+                        else
+                        {
+                            Output[j] = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (states[j - 1] == states[j + 1])
+                        {
+                            Output[j] = 0;
+                        }
+                        else
+                        {
+                            Output[j] = 1;
+                        }
+                    }
+                }
+                Array.Copy(Output, states, size);
+            }
+            return Output;
+        }
+
+        public static int GCD(int[] Arr, int num)
+        {
+            return Arr.Aggregate(CalculateGCD);
+        }
+        private static int CalculateGCD(int a, int b)
+        {
+            return b == 0 ? a : CalculateGCD(b, a % b);
+        }
+    }
+}
---
+++ CSConsoleTest/Amazon_Test/Demo_Test.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CSConsole.Amazon;
+
+namespace CSConsoleTest.Amazon_Test
+{
+    public class Demo_Test
+    {
+        // ==========================================================================================
+        // Cell Compete
+        // ==========================================================================================
+        [Fact]
+        public void cellComplete1()
+        {
+            //Arrange
+            int[] value = new int[] { 1, 0, 0, 0, 0, 1, 0, 0 };
+            int Iterations = 1;
+            int[] expectedResult = new int[] { 0, 1, 0, 0, 1, 0, 1, 0 };
+
+            //Act 
+            int[] result = Demo.cellComplete(value, Iterations);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void cellComplete2()
+        {
+            //Arrange
+            int[] value = new int[] { 1, 1, 1, 0, 1, 1, 1, 1 };
+            int Iterations = 2;
+            int[] expectedResult = new int[] { 0, 0, 0, 0, 0, 1, 1, 0 };
+
+            //Act 
+            int[] result = Demo.cellComplete(value, Iterations);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void cellComplete3()
+        {
+            //Arrange
+            int[] value = new int[] { 1, 1, 1, 0, 1, 1, 1, 1 };
+            int Iterations = 3;
+            int[] expectedResult = new int[] { 0, 0, 0, 0, 1, 1, 1, 1 };
+
+            //Act 
+            int[] result = Demo.cellComplete(value, Iterations);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void cellComplete4()
+        {
+            //Arrange
+            int[] value = new int[] { 1, 1, 1, 0, 1, 1, 1, 1 };
+            int Iterations = 4;
+            int[] expectedResult = new int[] { 0, 0, 0, 1, 1, 0, 0, 1 };
+
+            //Act 
+            int[] result = Demo.cellComplete(value, Iterations);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        // ==========================================================================================
+        // Greatest Common Divisor
+        // ==========================================================================================
+
+        [Fact]
+        public void GCD1()
+        {
+            //Arrange
+            int[] Array = new int[] { 2, 3, 4, 5, 6 };
+            int nums = 5;
+            int expectedResult = 1;
+
+            //Act 
+            int result = Demo.GCD(Array, nums);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void GCD2()
+        {
+            //Arrange
+            int[] Array = new int[] { 2, 4, 6, 8, 10 };
+            int nums = 5;
+            int expectedResult = 2;
+
+            //Act 
+            int result = Demo.GCD(Array, nums);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
